008bc2bce3c888430df20673e77f698d08bed1ea
Yiyuan Li <yy@eyuan.me>
2024-09-18 15:31:37 +0800

    add HTTP200 response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -6,8 +7,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
