ba8c18579afcffaee8b2775416cc07dd4457f2a1
Yiyuan Li <yy@eyuan.me>
2024-09-22 21:10:19 +0800

    add response body
    will response with /echo/{str}

--- src/main.rs
+++ src/main.rs
@@ -23,16 +23,40 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
+
+    // unwrap() is used, which will panic if there's an error
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let path = request_line.split_whitespace().nth(1).unwrap_or("/");
+    // Read the index 1, if it dosent exist, default to "/"
+    let url = request_line.split_whitespace().nth(1).unwrap_or("/");
 
-    let status = if path == "/" {
-        "200 OK"
+    if url.starts_with("/echo/") {
+        // Extract the string afther "/echo/"
+        let echo_string = &url[6..];
+
+        let content_len = echo_string.len();
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\n\
+            Content-Type: text/plain\r\n\
+            Content-Length: {}\r\n\
+            \r\n\
+            {}",
+            content_len, echo_string
+        );
+
+        // Write the response back to the TCP stream
+        stream.write_all(response.as_bytes()).unwrap();
     } else {
-        "404 Not Found"
-    };
+        let status = if url == "/" {
+            "200 OK"
+        } else {
+            "404 Not Found"
+        };
 
-    let response = format!("HTTP/1.1 {}\r\n\r\n", status);
-    stream.write_all(response.as_bytes()).unwrap();
+        let response = format!("HTTP/1.1 {}\r\n\r\n", status);
+
+        // Write the response back to the TCP stream
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
